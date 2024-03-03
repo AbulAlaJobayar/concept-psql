8fdd62e705d9f39ba34f5204b240721a1e9f22fb
Abul Ala Jobayar <jobayar59@gmail.com>
2024-03-03 22:32:00 +0600

    alter keyword

--- main.sql
+++ main.sql
@@ -1,10 +1,28 @@
 -- Active: 1709236027427@@127.0.0.1@5432@ph@public
+--create table
 CREATE TABLE person (
-        id SERIAL NOT NULL,
-        name VARCHAR(50) ,
-        age INT,
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        age INT check (age>0),
         profession VARCHAR(100),
         isActive BOOLEAN,
-        dob DATE,
+        dob DATE check (dob > current_date),
         rating NUMERIC(3,2)
 )
+
+--delate table
+DROP Table persons
+
+--use ALTER
+
+ALTER Table person
+RENAME to persons
+
+ALTER TABLE persons
+ADD COLUMN email VARCHAR(50)
+
+
+ALTER TABLE persons
+RENAME COLUMN dob to date_of_birth
+
+SELECT * from persons
